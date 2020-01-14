15a692b4695b4be338bb611003966ec3c6b3c452
Aleksandr Skoriy <askoriy@users.noreply.github.com>
2020-01-14 17:33:17 +0200

    Update server.go

--- simpleserver/server.go
+++ simpleserver/server.go
@@ -8,6 +8,10 @@ import (
         "strings"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
         log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
         target := os.Getenv("TARGET")
@@ -15,6 +19,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
                 target = "World"
         }
 
+        enableCors(&w)
+        
         var request []string
         url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
         request = append(request, url)
