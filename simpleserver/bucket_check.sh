032ec26817de30303a4a6919e11fe440e6de5870
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro.local>
2020-01-19 18:21:52 +0200

    bucket-pipeline

---
+++ simpleserver/bucket_check.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+PROJECT_ID=${1}
+REGION=${2}
+BUCKET_NAME=${3}
+
+gsutil mb -p ${PROJECT_ID} -l ${REGION} -c standard -b on gs://${BUCKET_NAME}
+if [ $? -eq 0 ]; then
+    echo "bucket is created"
+else
+   echo "Name's been taken...add some sufix"
+   gsutil mb -p ${PROJECT_ID} -l ${REGION} -c standard -b on gs://${BUCKET_NAME}-1234a
