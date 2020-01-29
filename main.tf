0a31f9df71ec026a5ccdbaee31a0f287af5ec6f1
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro.local>
2020-01-29 18:50:59 +0200

    debug

--- main.tf
+++ main.tf
@@ -3,13 +3,6 @@ locals {
   region  = "europe-west1"
 }
 
-provider "google-beta" {
-  version     = "~> 3.5"
-  project     = local.project
-  region      = local.region 
-  credentials = "key.json"
-}
-
 resource "google_identity_platform_tenant" "tenant1" {
   provider     = google-beta
   display_name = "Tenant-1"
