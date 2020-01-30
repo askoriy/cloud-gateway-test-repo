9bcd4f4901c8aa51d5fd6ad91535d41d6e4f7aaa
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro.local>
2020-01-30 11:34:54 +0200

    add key

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 locals {
-  project = "gcp-examples-staging-c693"
-  region  = "europe-west1"
+  project = "main-105"
+  region  = "us-central1"
+}
+
+provider "google-beta" {
+  version     = "~> 3.5"
+  project     = local.project
+  region      = local.region 
+  credentials = "key.json"
 }
 
 resource "google_identity_platform_tenant" "tenant1" {
   provider     = google-beta
   display_name = "Tenant-1"
-  project      = local.project   
-  credentials  =  "key.json"
+  project      = local.project     
 }
