de5f5835c4b5ec854c0bb326ba60379a36eae726
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro.local>
2020-01-30 14:50:48 +0200

    add second tenant

--- frontend-multi-tenant/terraform/main.tf
+++ frontend-multi-tenant/terraform/main.tf
@@ -18,20 +18,10 @@ provider "google-beta" {
 
 resource "google_identity_platform_tenant" "tenant_tf" {
   provider     = google-beta
-  display_name = "Tenant-terraform"
-  project      = local.project     
-}
-
-resource "google_identity_platform_tenant" "tenant_tf_2" {
-  provider     = google-beta
-  display_name = "Tenant-terraform-2"
+  for_each     = var.tenant_names
   project      = local.project     
 }
 
 output "tenant_name" {
   value = google_identity_platform_tenant.tenant_tf.name
-}
-
-output "tenant_name_2" {
-  value = [google_identity_platform_tenant.*.name]
 }
