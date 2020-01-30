b6da55093e657c61bb449130833bae813ae0f559
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro.local>
2020-01-30 12:05:52 +0200

    final key test

---
+++ frontend-multi-tenant/terraform/main.tf
@@ -0,0 +1,16 @@
+locals {
+  project = "main-105"
+  region  = "us-central1"
+}
+
+provider "google-beta" {
+  version     = "~> 3.5"
+  project     = local.project
+  region      = local.region 
+}
+
+resource "google_identity_platform_tenant" "tenant1" {
+  provider     = google-beta
+  display_name = "Tenant-1"
+  project      = local.project     
+}
